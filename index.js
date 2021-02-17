ba8a78664d8cf85167d59e413a42e1c935e4b6b6
hussein6065 <fuseinial@gmail.com>
2021-02-17 23:49:55 +0000

    Added url to the api to the index.js file

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function getSomething() {
 	document.getElementById('Hussein').innerHTML = 'JAmeas';
+
+	// Api to use
+	// https://www.coinapi.io/pricing?apikey for crypto
+
+	// https://rapidapi.com/natkapral/api/currency-converter5 for the currency convertion
 }
