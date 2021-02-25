f1f975a141675b2b13a85a1d1dd8a2d4be725996
hussein6065 <fuseinial@gmail.com>
2021-02-25 13:34:58 +0000

    initiated the Apis

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-function getSomething() {
-	document.getElementById('Hussein').innerHTML = 'JAmeas';
-
-	// Api to use
-	// https://www.coinapi.io/pricing?apikey for crypto
-
-	// https://rapidapi.com/natkapral/api/currency-converter5 for the currency convertion
+// function getSomething() {
+// 	// Api to use
+// 	// https://www.coinapi.io/pricing?apikey for crypto
+// 	// https://rapidapi.com/natkapral/api/currency-converter5 for the currency convertion
+// }
+var crypto_USD = null;
+var currency_USD = null;
+function getCurrencyRate() {
+	fetch(
+		'https://api.exchangerate.host/latest?base=NGN&symbols=USD,EUR,GHS,GBP,AUD',
+		{
+			method: 'GET',
+		}
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			currency_USD = data.rates;
+			console.log(currency_USD);
+		});
 }
+
+function getCoinRate() {
+	fetch(
+		'https://api.exchangerate.host/latest?base=USD&symbols=BTC,ETH,BNB,BCH,DOGE,DOT,ADA&source=crypto',
+		{
+			method: 'GET',
+		}
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			crypto_USD = data.rates;
+			console.log(crypto_USD);
+		});
+}
+
+window.addEventListener(
+	'DOMContentLoaded',
+	() => {
+		getCoinRate();
+		getCurrencyRate();
+	},
+	false
+);
